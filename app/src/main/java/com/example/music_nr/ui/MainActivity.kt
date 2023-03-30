1209c00256358923e9ecea88cf1a3e201082a296
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-03-30 21:09:17 +0200

    run from main activity :)

--- app/src/main/java/com/example/music_nr/ui/MainActivity.kt
+++ app/src/main/java/com/example/music_nr/ui/MainActivity.kt
@@ -2,16 +2,29 @@ package com.example.music_nr.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.music_nr.data.DataManger
+import com.example.music_nr.data.HomeItem
+import com.example.music_nr.data.HomeType
 import com.example.music_nr.databinding.ActivityMainBinding
+import com.example.music_nr.ui.adapter.MainAdapter
 
-class MainActivity : AppCompatActivity(){
-
+class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupMainViewRecyclerView()
     }
 
+    private fun setupMainViewRecyclerView() {
+        val items = mutableListOf<HomeItem<Any>>()
+        items.add(HomeItem("Search About Song", HomeType.ITEM_SEARCH))
+        items.add(HomeItem(DataManger.songs.map { it.image }, HomeType.ITEM_TRENDING_RECYCLER_VIEW))
+        items.add(HomeItem("", HomeType.ITEM_CHIPS))
+        items.addAll(DataManger.songs.map { it.toHomeItem() })
+        val adapter = MainAdapter(items)
+        binding.recyclerMainActivity.adapter = adapter
+    }
 }
